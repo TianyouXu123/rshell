ba273e3b978c82783acc2df1dead713585a68456
kurouburusuto <zeroxty00@gmail.com>
2017-05-09 15:54:47 -0700

    Update commented_command.sh

--- test/commented_command.sh
+++ test/commented_command.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #tests commands with comments
-#tests exit and commands with exit
 
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
-echo "----------"
-str="echo hello world # this is a comment; exit # quit"
+echo "test"
+str="echo hello world # this is a comment; exit"
 
 echo ${str}
 {
@@ -13,4 +12,4 @@ echo ${str}
 	sleep 1
 } | ./test
 
-rm a.out
+rm test
