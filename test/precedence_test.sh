1b0620d7796db43c62e17c2afd773225cdca7f20
TianyouXu123 <zeroxty00@gmail.com>
2017-05-31 11:08:25 -0700

    Update precedence_test.sh

--- test/precedence_test.sh
+++ test/precedence_test.sh
@@ -6,13 +6,19 @@ echo "precedence_test:"
 str1="echo A && echo B || echo C && echo D"
 str2="echo A && echo B || (echo C && echo D || echo E)"
 str3="exit"
-echo ${str}
+echo ${str1}
 {
 	echo ${str1}
 	sleep 1
-  echo ${str2}
-  sleep 1
-  echo ${str3}	
+} | ./test
+echo ${str2}
+{
+	echo ${str2}
+	sleep 1
+} | ./test
+echo ${str3}
+{
+	echo ${str3}
 	sleep 1
 } | ./test
 
