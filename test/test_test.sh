811315303cb29b4d84fc95130eeac20ca683399e
TianyouXu123 <zeroxty00@gmail.com>
2017-05-31 11:21:57 -0700

    Update test_test.sh

--- test/test_test.sh
+++ test/test_test.sh
@@ -5,7 +5,7 @@ g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
 echo "test command test:"
 
-str="test -e test/test_test.sh && echo "File found! && [ -e test/123.txt ] || echo "File not found! && exit"
+str="test -e test/test_test.sh && echo "File found!"; [ -e test/123.txt ] || echo "File not found!"; exit"
 echo ${str}
 {
 	echo ${str}
