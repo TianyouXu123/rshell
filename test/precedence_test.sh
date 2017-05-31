de8a2742f7cd8474ceaf7ecf9e24c89285d45d6a
TianyouXu123 <zeroxty00@gmail.com>
2017-05-31 11:22:05 -0700

    Update precedence_test.sh

--- test/precedence_test.sh
+++ test/precedence_test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+#tests single commands
 
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
-echo "precedence_test:"
-str="echo A && echo B || echo C && echo D; echo A && echo B || (echo C && echo D || echo E); exit"
+echo "test command test:"
+
+str="test -e test/test_test.sh && echo "File found!"; [ -e test/123.txt ] || echo "File not found!"; exit"
 echo ${str}
 {
 	echo ${str}
