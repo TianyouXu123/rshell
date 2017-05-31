038fd586d66046bf38ac31dc6c4ef97c88e2dbbe
TianyouXu123 <zeroxty00@gmail.com>
2017-05-31 11:20:43 -0700

    Update precedence_test.sh

--- test/precedence_test.sh
+++ test/precedence_test.sh
@@ -3,21 +3,7 @@
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
 echo "precedence_test:"
-str="echo A && echo B || echo C && echo D"
-echo ${str}
-{
-	echo ${str}
-	sleep 1
-} | ./test
-
-str="echo A && echo B || (echo C && echo D || echo E)"
-echo ${str}
-{
-	echo ${str}
-	sleep 1
-} | ./test
-
-str="exit"
+str="echo A && echo B || echo C && echo D && echo A && echo B || (echo C && echo D || echo E); exit"
 echo ${str}
 {
 	echo ${str}
