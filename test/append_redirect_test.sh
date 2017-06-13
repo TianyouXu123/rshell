d2113ee318baa23678db1be35de2dbca4f2aa308
TianyouXu123 <zeroxty00@gmail.com>
2017-06-13 01:29:27 -0700

    Create append_redirect_test.sh

--- test/append_redirect_test.sh
+++ test/append_redirect_test.sh
@@ -3,7 +3,7 @@
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
 echo "append_redirect_test:"
-str="cat < test/test.txt | tr A-Z a-z | tee test/output4.txt | tr a-z A-Z >> test/output5.txt && exit"
+str="cat < test.txt | tr A-Z a-z | tee output4.txt | tr a-z A-Z >> output5.txt && exit"
 
 echo ${str}
 {
