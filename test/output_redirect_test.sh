b7cab79ac57b0584d5b43e1286d6a60cc0200cfa
TianyouXu123 <zeroxty00@gmail.com>
2017-06-13 00:44:55 -0700

    Update output_redirect_test.sh

--- test/output_redirect_test.sh
+++ test/output_redirect_test.sh
@@ -3,7 +3,7 @@
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
 echo "output_redirect_test:"
-str="cat < test/test.txt | tr a-z A-Z > output1.txt && exit"
+str="cat < test/test.txt | tr a-z A-Z > test/output1.txt && exit"
 
 echo ${str}
 {
