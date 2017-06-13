f9e593e6dd941062c0d7d214c76f5130776ed9db
TianyouXu123 <zeroxty00@gmail.com>
2017-06-13 00:39:45 -0700

    Create pipe_test.sh

--- test/pipe_test.sh
+++ test/pipe_test.sh
@@ -3,7 +3,7 @@
 g++ -Wall -Werror -ansi -pedantic ../src/main.c -o test
 
 echo "pipe_test:"
-str="$ cat < src/main.c | tr A-Z a-z | tee output1.txt | tr a-z A-Z > output2.txt && exit"
+str="$ cat < test/test.txt | tr A-Z a-z | tee output2.txt | tr a-z A-Z > output3.txt && exit"
 
 echo ${str}
 {
